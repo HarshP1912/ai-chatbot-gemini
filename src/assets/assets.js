df65175e9e4ec2d7e22cf06687a8e7fa2fa71d08
HarshP1912 <harshpatne22@gmail.com>
2024-05-16 20:38:24 +0530

    initial commit

---
+++ src/assets/assets.js
@@ -0,0 +1,9 @@
+import user_icon from "./user_icon.png";
+import send_icon from "./send_icon.png";
+import chatbot_icon from "./chatbot.jpg";
+
+export const assets = {
+  user_icon,
+  send_icon,
+  chatbot_icon,
+};

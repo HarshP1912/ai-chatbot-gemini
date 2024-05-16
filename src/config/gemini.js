d60ff97e0b00c74fe9405f9b25b1dac91f29113b
HarshP1912 <118930582+HarshP1912@users.noreply.github.com>
2024-05-16 20:39:19 +0530

    Update gemini.js

--- src/config/gemini.js
+++ src/config/gemini.js
@@ -8,7 +8,7 @@ import {
 } from "@google/generative-ai";
 
 const MODEL_NAME = "gemini-1.0-pro";
-const API_KEY = "AIzaSyCGLRZLOq7vOaunQXkgctFt3AN0EqiZHwM";
+const API_KEY = "YOUR_API_KEY";
 
 async function runChat(prompt) {
   const genAI = new GoogleGenerativeAI(API_KEY);
